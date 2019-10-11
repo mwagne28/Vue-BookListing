a6b18fe041ea50ae57dbab28de10a02c9791fb8d
Mike Wagner <Michael.w.wagner@daugherty.com>
2019-10-11 14:46:37 -0500

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,11 +4,13 @@
         <ul>
             <book-item v-for="book in books" :book=book></book-item>
         </ul>
+        <book-form @addbook='appendBook'></book-form>
     </div>
 </template>
 
 <script>
     import BookItem from './BookItem';
+    import BookForm from './BookForm';
 
     export default {
         name: "BookList",
@@ -24,6 +26,12 @@
         },
         components: {
             BookItem,
+            BookForm,
+        },
+        methods: {
+            appendBook(bookTitle, bookAuthor) {
+                this.books.push({title: bookTitle, author: bookAuthor})
+            }
         }
     }
 </script>
